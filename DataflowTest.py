5d712ccd88ab1b6f337817e672e87fd465d489ac
Neha Supe <37761651+nehasupe@users.noreply.github.com>
2020-10-15 04:16:27 -0400

    Update DataflowTest.py

--- DataflowTest.py
+++ DataflowTest.py
@@ -28,6 +28,19 @@ class ReadFile(beam.DoFn):
 				currency = prod.get("currency")
 				clear_data.append([product_id, product_name, product_price, currency, created_at])
 		yield clear_data
+class WriteCSVFile(beam.DoFn):
+	def __init__(self, bucket_name):
+		self.bucket_name = bucket_name
+
+	def start_bundle(self):
+		self.client = storage.Client()
+
+	def process(self, mylist):
+		df = pd.DataFrame(mylist, columns={'product_id': str, 'product_name': str, 'product_price': str, 'currency': currency, 'created_at': str})
+		bucket = self.client.get_bucket(self.bucket_name)
+		bucket.blob(f'csv_exports.csv').upload_from_string(df.to_csv(index=False), 'text/csv')
+
+
 
 
 class DataflowOptions(PipelineOptions):
